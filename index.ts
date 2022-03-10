60e779eeefac125dc3e30ddf216678a9d5c483e8
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-03-11 03:07:23 +0900

    Added dotenv, and error logging, fixed object key

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
-import { GetObjectCommand, GetObjectCommandOutput, PutObjectCommand, S3, S3Client } from '@aws-sdk/client-s3';
+import 'dotenv/config'
+import express from 'express'
+import { Request, Response, NextFunction } from 'express'
+import { GetObjectCommand, GetObjectCommandOutput, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { fromSus } from "sonolus-pjsekai-engine"
-import { gzipPromise, streamToString } from './utils';
-import { Readable } from 'stream';
-import crypto from 'crypto';
+import { gzipPromise, streamToString } from './utils'
+import { Readable } from 'stream'
+import crypto from 'crypto'
 
+interface S3Error {
+    Code: string
+    $metadata: {
+        httpStatusCode: string
+    }
+}
+
+interface PostConvert {
+    hash: string
+}
+
+
+const app = express()
 const bucket: string = process.env.S3_BUCKET!
-
-const app: Express = express();
-const s3: S3Client = new S3Client({
+const s3 = new S3Client({
     credentials: {
         accessKeyId: process.env.S3_KEY!,
         secretAccessKey: process.env.S3_SECRET!,
@@ -16,56 +29,74 @@ const s3: S3Client = new S3Client({
     endpoint: process.env.S3_ENDPOINT,
     forcePathStyle: true,
     region: process.env.S3_REGION || 'unknown',
-});
+})
 
 app.use(express.json())
 
+const isError = (d: any): d is S3Error => {
+    if (!d) return false
+    if (d.Code && typeof d.Code === "string" && d.$metadata && typeof d.$metadata === "object") {
+      if (
+        typeof d.$metadata.httpStatusCode === "number"
+      ) {
+        return true
+      }
+    }
+    return false
+}
 
 app.post("/convert", async (req: express.Request, res: express.Response) => {
-    const { hash }: PostConvert = req.body;
-    let content: GetObjectCommandOutput;
+    const { hash }: PostConvert = req.body
+    let content: GetObjectCommandOutput
     try {
         content = await s3.send(
             new GetObjectCommand({
                 Bucket: bucket,
-                Key: `/SusFile/${hash}`
+                Key: `SusFile/${hash}`
             })
         )
-    } catch {
+    } catch (e: unknown) {
+        if (isError(e)) {
+            const err: S3Error = e
+            console.log(`S3 GetObject Error: ${hash} / ${err.$metadata.httpStatusCode} / ${err.Code}`)
+        }
         res.status(404).send({
             error: "File not found",
             code: "file_not_found"
         })
-        return;
+        return
     }
-    const body = content.Body as Readable;
-    const data = await fromSus(await streamToString(body));
-    const compressed = await gzipPromise(JSON.stringify(data));
-    const compressedBuffer = Buffer.from(compressed);
-    const compressedHash = crypto.createHash('sha1').update(compressedBuffer).digest('hex');
-    await s3.send(
-        new PutObjectCommand({
-            Bucket: bucket,
-            Key: `/LevelData/${compressedHash}`,
-            Body: compressedBuffer
-        })
-    )
+    const body = content.Body as Readable
+    const data = await fromSus(await streamToString(body))
+    const compressed = await gzipPromise(JSON.stringify(data))
+    const compressedBuffer = Buffer.from(compressed)
+    const compressedHash = crypto.createHash('sha1').update(compressedBuffer).digest('hex')
+    try {
+        await s3.send(
+            new PutObjectCommand({
+                Bucket: bucket,
+                Key: `LevelData/${compressedHash}`,
+                Body: compressedBuffer
+            })
+        )
+    } catch (e: unknown) {
+        if (isError(e)) {
+            const err: S3Error = e
+            console.log(`S3 PutObject Error: ${err.$metadata.httpStatusCode} / ${err.Code}`)
+        }
+    }
     res.json({
         hash: compressedHash
     })
 })
 
 app.use((_req: Request, res: Response, _next: NextFunction) => {
-    res.status(404).json({ message: 'Not found' });
+    res.status(404).json({ message: 'Not found' })
 })
 
 if (require.main === module) {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3000
     app.listen(+port, "0.0.0.0", () => {
-        console.log(`Server listening on port ${port}`);
-    });
-}
-
-interface PostConvert {
-    hash: string
+        console.log(`Server listening on port ${port}`)
+    })
 }
